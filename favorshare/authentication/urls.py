7fc3ab34f5f44ef5f4cfc8c29ace63adfa351331
Xabier Larrakoetxea <slok69@gmail.com>
2014-06-07 12:10:37 +0200

    Added authentication app

---
+++ favorshare/authentication/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, url
+from django.contrib.auth.views import login, logout
+from . import views
+
+urlpatterns = patterns('',
+
+    url(r'^login/$',
+        view=login,
+        name="login",
+        kwargs={'template_name': 'authentication/login.html'}
+    ),
+
+    url(r'^logout/$',
+        view=logout,
+        name="logout",
+        kwargs={'next_page': 'homepage:index'}
+    ),
+
+)
--- favorshare/favorshare/urls.py
+++ favorshare/favorshare/urls.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from profiles import urls as profile_urls
 from homepage import urls as homepage_urls
+from authentication import urls as authentication_urls
 
 
 admin.autodiscover()
@@ -23,7 +24,13 @@ urlpatterns = patterns('',
     url(r'p/', include(profile_urls,
         namespace="profiles",
         app_name="profiles"),
-    )
+    ),
+
+    # Authentication
+    url(r'a/', include(authentication_urls,
+        namespace="authentication",
+        app_name="authentication"),
+    ),
 
 )
 
