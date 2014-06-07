506b29bcad440222c30f8a880d55ce3ea9513647
Xabier Larrakoetxea <slok69@gmail.com>
2014-06-07 16:45:05 +0200

    Modified styles

--- favorshare/homepage/views.py
+++ favorshare/homepage/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.base import TemplateView
 
+from core.mixins import LoginRequiredMixin
 
-class HomepageIndexView(TemplateView):
+
+class HomepageIndexView(LoginRequiredMixin, TemplateView):
 
     template_name = "homepage/index.html"
