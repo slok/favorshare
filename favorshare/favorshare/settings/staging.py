603ad9c09d409c1122f64b3f122d686cb45df22e
Xabier Larrakoetxea <slok69@gmail.com>
2014-06-19 23:04:24 +0200

    some changes for deployments

--- favorshare/favorshare/settings/staging.py
+++ favorshare/favorshare/settings/staging.py
@@ -1,1 +1,31 @@
-from .base import *
+#from .base import *
+#
+####################################################
+#  This tempalte will be set in depoyment process  #
+####################################################
+
+#SECRET_KEY = ''
+#DEBUG = False
+#TEMPLATE_DEBUG = False
+#ALLOWED_HOSTS = []
+#
+#
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "",
+#        "NAME": "",
+#        "USER": "",
+#        "PASSWORD": "",
+#        "HOST": "",
+#        "PORT": "",
+#    }
+#}
+#
+#INTERNAL_IPS = (
+#)
+#
+#INSTALLED_APPS += (
+#)
+#
+#MIDDLEWARE_CLASSES += (
+#)
--- favorshare/favorshare/wsgi.py
+++ favorshare/favorshare/wsgi.py
@@ -8,7 +8,26 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "favorshare.settings")
+
+
+settings_prefix = "favorshare.settings"
+
+# Get settings
+settings = {
+    "staging": "staging",
+    "stage": "staging",
+    "samdbox": "staging",
+    "production": "production",
+    "prod": "production",
+    "local": "local",
+    "vagrant": "local",
+}
+
+env = os.environ['SYSTEM_ENV']
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", ".".join(
+    (settings_prefix, settings.get(env, "local")))
+)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
